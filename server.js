73a77129048a66bcbb49dc5b842ef9089214283f
Wesley Berry <wesleyberry52@gmail.com>
2020-01-16 17:00:07 -0600

    set up server file to serve html

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.sendFile('/index.html' , { root : __dirname});
+});
+
+app.listen(PORT, () => console.log(`Running on PORT ${PORT}`));
